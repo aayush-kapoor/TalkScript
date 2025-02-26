a791a356df12c1f232328637d65d8acf4a592b17
Aayush Kapoor <aayushkapoor34@gmail.com>
2025-02-25 23:07:30 -0500

    eslint change

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
         "plugin:react-hooks/recommended"
     ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
     "rules": {
         "no-unused-vars": "warn",
         "no-sparse-arrays": "warn",
@@ -21,5 +29,4 @@ module.exports = {
             "version": "detect"
         }
     }
-    
 }; 
